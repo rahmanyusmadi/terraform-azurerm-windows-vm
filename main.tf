2d01f60d4f34068917ba80bb30aa19d93c63ffd9
Abdul Rahman Yusmadi <31083956+yusmadi@users.noreply.github.com>
2019-11-13 15:03:43 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ provider "azurerm" {}
 data "azurerm_resource_group" "main" {
   name = "rahman-terraform-azurerm-windows"
 }
+
+resource "azurerm_virtual_network" "main" {
+    name                = "rahman-terraform-azurerm-windows"
+    address_space       = ["10.0.0.0/16"]
+    location            = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+}
