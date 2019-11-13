b4018e0233eda773bc5e97831ad507b1ba7079b8
Abdul Rahman Yusmadi <31083956+yusmadi@users.noreply.github.com>
2019-11-13 16:06:40 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_virtual_network" "main" {
     name                = "vnet1"
     address_space       = ["10.0.0.0/16"]
-    location            = data.azurerm_resource_group.main.location
-    resource_group_name = data.azurerm_resource_group.main.name
+    location            = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
 
     tags = {
         label = var.name
