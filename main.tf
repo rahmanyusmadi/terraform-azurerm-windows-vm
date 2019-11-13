861bba3aac9f71b5e755cf28539819c19d74f5e3
Abdul Rahman Yusmadi <31083956+yusmadi@users.noreply.github.com>
2019-11-13 18:09:18 +0800

    Remove additional_unattend_config

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ variable "location" {}
 variable "prefix" {}
 
 resource "azurerm_resource_group" "main" {
-  name = var.prefix
+  name     = var.prefix
   location = var.location
 }
 
 resource "azurerm_virtual_network" "main" {
-  name        = "vnet1"
-  address_space     = ["10.0.0.0/16"]
-  location      = azurerm_resource_group.main.location
+  name                = "vnet1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
   tags = {
@@ -23,18 +23,18 @@ resource "azurerm_virtual_network" "main" {
 }
 
 resource "azurerm_subnet" "main" {
-  name         = "subnet1"
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix     = "10.0.2.0/24"
+  address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_public_ip" "main" {
-  name             = "pip1"
-  location           = azurerm_resource_group.main.location
-  resource_group_name      = azurerm_resource_group.main.name
-  allocation_method      = "Dynamic"
-  domain_name_label = var.prefix
+  name                = "pip1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+  domain_name_label   = var.prefix
 
   tags = {
     label = var.prefix
@@ -42,8 +42,8 @@ resource "azurerm_public_ip" "main" {
 }
 
 resource "azurerm_network_security_group" "main" {
-  name        = "nsg1"
-  location      = azurerm_resource_group.main.location
+  name                = "nsg1"
+  location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   
   tags = {
@@ -52,16 +52,16 @@ resource "azurerm_network_security_group" "main" {
 }
 
 resource "azurerm_network_interface" "main" {
-  name            = "nic1"
-  location          = azurerm_resource_group.main.location
-  resource_group_name     = azurerm_resource_group.main.name
-  network_security_group_id   = azurerm_network_security_group.main.id
+  name                      = "nic1"
+  location                  = azurerm_resource_group.main.location
+  resource_group_nam  e     = azurerm_resource_group.main.name
+  network_security_group_id = azurerm_network_security_group.main.id
 
   ip_configuration {
-    name              = "config1"
-    subnet_id           = azurerm_subnet.main.id
+    name                          = "config1"
+    subnet_id                     = azurerm_subnet.main.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id      = azurerm_public_ip.main.id
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 
   tags = {
@@ -70,23 +70,23 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name          = "vm1"
-  location        = azurerm_resource_group.main.location
+  name                  = "vm1"
+  location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size         = "Standard_DS1_v2"
+  vm_size               = "Standard_DS1_v2"
 
   storage_os_disk {
-    name        = "disk1"
-    caching       = "ReadWrite"
-    create_option   = "FromImage"
+    name              = "disk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
     managed_disk_type = "Premium_LRS"
   }
 
   storage_image_reference {
     publisher = "MicrosoftWindowsDesktop"
-    offer   = "Windows-10"
-    sku     = "19h1-pro"
+    offer     = "Windows-10"
+    sku       = "19h1-pro"
     version   = "latest"
   }
 
@@ -97,17 +97,8 @@ resource "azurerm_virtual_machine" "main" {
   }
 
   os_profile_windows_config {
-  provision_vm_agent    = true
-  enable_automatic_upgrades = true
-
-  # Auto-Login's required to configure WinRM
-  additional_unattend_config {
-    pass     = "oobeSystem"
-    component  = "Microsoft-Windows-Shell-Setup"
-    setting_name = "AutoLogon"
-    content    = "<AutoLogon><Password><Value>${var.password}</Value></Password><Enabled>true</Enabled><LogonCount>1</LogonCount><Username>${var.password}</Username></AutoLogon>"
-  }
-
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
   }
 
   tags = {
