0d37022b386c8b56e5e5c2af757ccfc236cedadd
ayusmadi <abdul-rahman.yusmadi@dxc.com>
2019-11-15 12:24:26 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -211,59 +211,45 @@ resource "azurerm_template_deployment" "main" {
 
   template_body = <<DEPLOY
 {
-  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
   "parameters": {
-    "storageAccountType": {
-      "type": "string",
-      "defaultValue": "Standard_LRS",
-      "allowedValues": [
-        "Standard_LRS",
-        "Standard_GRS",
-        "Standard_ZRS"
-      ],
-      "metadata": {
-        "description": "Storage Account type"
-      }
-    }
-  },
-  "variables": {
-    "location": "[resourceGroup().location]",
-    "storageAccountName": "[concat(uniquestring(resourceGroup().id), 'storage')]",
-    "publicIPAddressName": "[concat('myPublicIp', uniquestring(resourceGroup().id))]",
-    "publicIPAddressType": "Dynamic",
-    "apiVersion": "2015-06-15",
-    "dnsLabelPrefix": "terraform-acctest"
+    "location": {
+      "type": "String",
+    },
+    "virtualMachineName": {
+      "type": "String",
+    },
+    "autoShutdownStatus": {
+      "type": "String",
+    },
+    "autoShutdownTime": {
+      "type": "String",
+    },
+    "autoShutdownTimeZone": {
+      "type": "String",
+    },
   },
   "resources": [
     {
-      "type": "Microsoft.Storage/storageAccounts",
-      "name": "[variables('storageAccountName')]",
-      "apiVersion": "[variables('apiVersion')]",
-      "location": "[variables('location')]",
+      "type": "Microsoft.DevTestLab/schedules",
+      "apiVersion": "2017-04-26-preview",
+      "name": "[concat('shutdown-computevm-', parameters('virtualMachineName'))]",
+      "location": "[parameters('location')]",
+      "dependsOn": [
+          "[concat('Microsoft.Compute/virtualMachines/', parameters('virtualMachineName'))]"
+      ],
       "properties": {
-        "accountType": "[parameters('storageAccountType')]"
-      }
-    },
-    {
-      "type": "Microsoft.Network/publicIPAddresses",
-      "apiVersion": "[variables('apiVersion')]",
-      "name": "[variables('publicIPAddressName')]",
-      "location": "[variables('location')]",
-      "properties": {
-        "publicIPAllocationMethod": "[variables('publicIPAddressType')]",
-        "dnsSettings": {
-          "domainNameLabel": "[variables('dnsLabelPrefix')]"
-        }
+          "status": "[parameters('autoShutdownStatus')]",
+          "taskType": "ComputeVmShutdownTask",
+          "dailyRecurrence": {
+            "time": "[parameters('autoShutdownTime')]"
+          },
+          "timeZoneId": "[parameters('autoShutdownTimeZone')]",
+          "targetResourceId": "[resourceId('Microsoft.Compute/virtualMachines', parameters('virtualMachineName'))]",
       }
     }
-  ],
-  "outputs": {
-    "storageAccountName": {
-      "type": "string",
-      "value": "[variables('storageAccountName')]"
-    }
-  }
+  ]
 }
 DEPLOY
 
