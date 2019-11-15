1d0fa186e7586848388e371861ff13b34e9e9a1b
Abdul Rahman Yusmadi <31083956+yusmadi@users.noreply.github.com>
2019-11-15 08:42:24 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -189,7 +189,7 @@ resource "azurerm_dev_test_schedule" "main" {
   status = "Enabled"
 
   daily_recurrence {
-    time      = "0829"
+    time      = "0844"
   }
 
   time_zone_id = "Singapore Standard Time"
