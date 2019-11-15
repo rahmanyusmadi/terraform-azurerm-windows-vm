5d16227d5c86a3d186749ffda1e9190943c4cc95
ayusmadi <abdul-rahman.yusmadi@dxc.com>
2019-11-15 12:18:13 +0800

    fix

--- main.tf
+++ main.tf
@@ -209,14 +209,66 @@ resource "azurerm_template_deployment" "main" {
   name                = "${var.prefix}-template1"
   resource_group_name = azurerm_resource_group.main.name
 
-  template_body = file("auto-shutdown.json")
+  template_body = <<DEPLOY
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "storageAccountType": {
+      "type": "string",
+      "defaultValue": "Standard_LRS",
+      "allowedValues": [
+        "Standard_LRS",
+        "Standard_GRS",
+        "Standard_ZRS"
+      ],
+      "metadata": {
+        "description": "Storage Account type"
+      }
+    }
+  },
+  "variables": {
+    "location": "[resourceGroup().location]",
+    "storageAccountName": "[concat(uniquestring(resourceGroup().id), 'storage')]",
+    "publicIPAddressName": "[concat('myPublicIp', uniquestring(resourceGroup().id))]",
+    "publicIPAddressType": "Dynamic",
+    "apiVersion": "2015-06-15",
+    "dnsLabelPrefix": "terraform-acctest"
+  },
+  "resources": [
+    {
+      "type": "Microsoft.Storage/storageAccounts",
+      "name": "[variables('storageAccountName')]",
+      "apiVersion": "[variables('apiVersion')]",
+      "location": "[variables('location')]",
+      "properties": {
+        "accountType": "[parameters('storageAccountType')]"
+      }
+    },
+    {
+      "type": "Microsoft.Network/publicIPAddresses",
+      "apiVersion": "[variables('apiVersion')]",
+      "name": "[variables('publicIPAddressName')]",
+      "location": "[variables('location')]",
+      "properties": {
+        "publicIPAllocationMethod": "[variables('publicIPAddressType')]",
+        "dnsSettings": {
+          "domainNameLabel": "[variables('dnsLabelPrefix')]"
+        }
+      }
+    }
+  ],
+  "outputs": {
+    "storageAccountName": {
+      "type": "string",
+      "value": "[variables('storageAccountName')]"
+    }
+  }
+}
+DEPLOY
 
   parameters = {
-    "location"             = azurerm_resource_group.main.location
-    "virtualMachineName"   = azurerm_virtual_machine.main.name
-    "autoShutdownStatus"   = var.autoShutdownStatus
-    "autoShutdownTime"     = var.autoShutdownTime
-    "autoShutdownTimeZone" = var.autoShutdownTimeZone
+    "storageAccountType" = "Standard_GRS"
   }
 
   deployment_mode = "Incremental"
