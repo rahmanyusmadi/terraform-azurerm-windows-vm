23ba1bc5e7c572de83bcea34f3268f56c04efa42
ayusmadi <abdul-rahman.yusmadi@dxc.com>
2019-11-15 16:36:25 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# terraform-azurerm-windows
-A Terraform module to create a Windows virtual machine in Azure
+# Windows Azure Module
+A Terraform module to create a Windows virtual machine in Azure.
+
+* By default create a single [Windows 10, version 1903 IT Pro](https://docs.microsoft.com/en-us/windows/whats-new/whats-new-windows-10-version-1903)
+* Generate password and store secret in key vault
+* Allow only one source IP address to access remotely
+* Auto-shutdown virtual machine by default
+
+#### The simplest example how to use this module
+```
+module "windows" {
+  source = "yusmadi/windows/azurerm"
+}
+```
+
+#### Full example how to use this module
+```
+module "windows" {
+  source               = "yusmadi/windows/azurerm"
+  prefix               = "mywindows"
+  location             = "southeastasia"
+  address_space        = ["10.0.0.0/16"]
+  address_prefix       = "10.0.2.0/24"
+  my_public_ip_address = "13.15.17.19"
+  vm_size              = "Standard_DS1_v2"
+  publisher            = "MicrosoftWindowsDesktop"
+  offer                = "Windows-10"
+  sku                  = "19h1-pro"
+  image_version        = "latest"
+}
+```
+
+#### Reference
+
+* [What is module?](https://www.terraform.io/docs/configuration/modules.html)
+* [How can I improve this module?](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/proposing-changes-to-your-work-with-pull-requests)
+* [How is this module versioned?](https://semver.org/)
+* [What are the available size of virtual machines?](https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-size-specs/)
---
+++ main.tf
@@ -0,0 +1,269 @@
+terraform {
+  required_version = "~> 0.12.0"
+
+  required_providers {
+    azurerm = "~> 1.36.0"
+  }
+}
+
+data "azurerm_client_config" "main" {}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.prefix
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet1"
+  address_space       = var.address_space
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = var.address_prefix
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-pip1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+  domain_name_label   = var.prefix
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-nsg1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+resource "azurerm_network_security_rule" "remote_desktop" {
+  name                        = "Remote Access"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "${var.my_public_ip_address}/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
+resource "azurerm_network_interface" "main" {
+  name                      = "${var.prefix}-nic1"
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  network_security_group_id = azurerm_network_security_group.main.id
+
+  ip_configuration {
+    name                          = "${var.prefix}-config1"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm1"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = var.vm_size
+
+  delete_os_disk_on_termination = true
+
+  storage_os_disk {
+    name              = "${var.prefix}-disk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.image_version
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}"
+    admin_username = "${var.prefix}-user"
+    admin_password = random_password.password.result
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
+  }
+
+  /*
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [ data.azurerm_client_config.main.client_id ]
+  }
+  */
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+resource "azurerm_key_vault" "main" {
+  name                        = "${var.prefix}-vault"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.main.tenant_id
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.main.tenant_id
+    object_id = data.azurerm_client_config.main.object_id
+
+    secret_permissions = [
+      "get",
+      "set",
+      "delete",
+    ]
+  }
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+resource "azurerm_key_vault_secret" "password" {
+  name         = "${var.prefix}-password"
+  value        = random_password.password.result
+  key_vault_id = "${azurerm_key_vault.main.id}"
+
+  tags = {
+    label = var.prefix
+  }
+}
+
+/* auto-shutdown doesn't work at the moment. refer terraform-provider-azurerm issues with service/devtestlabs label
+resource "azurerm_dev_test_lab" "main" {
+  name                = "YourDevTestLab"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+}
+
+resource "azurerm_dev_test_schedule" "main" {
+  name                = "shutdown-compute-${var.prefix}-vm1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  lab_name            = azurerm_dev_test_lab.main.name
+  
+  status = "Enabled"
+
+  daily_recurrence {
+    time      = "0852"
+  }
+
+  time_zone_id = "Singapore Standard Time"
+  task_type    = "ComputeVmShutdownTask"
+
+  notification_settings {
+  }
+
+  tags = {
+    label = var.prefix
+  }
+}
+*/
+
+resource "azurerm_template_deployment" "main" {
+  name                = "${var.prefix}-template1"
+  resource_group_name = azurerm_resource_group.main.name
+
+  template_body = <<DEPLOY
+{
+    "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "location": {
+            "type": "String"
+        },
+        "virtualMachineName": {
+            "type": "String"
+        },
+        "autoShutdownStatus": {
+            "type": "String"
+        },
+        "autoShutdownTime": {
+            "type": "String"
+        },
+        "autoShutdownTimeZone": {
+            "type": "String"
+        }
+    },
+    "variables": {
+    },
+    "resources": [
+        {
+            "type": "Microsoft.DevTestLab/schedules",
+            "apiVersion": "2017-04-26-preview",
+            "name": "[concat('shutdown-computevm-', parameters('virtualMachineName'))]",
+            "location": "[parameters('location')]",
+            "properties": {
+                "status": "[parameters('autoShutdownStatus')]",
+                "taskType": "ComputeVmShutdownTask",
+                "dailyRecurrence": {
+                    "time": "[parameters('autoShutdownTime')]"
+                },
+                "timeZoneId": "[parameters('autoShutdownTimeZone')]",
+                "targetResourceId": "[resourceId('Microsoft.Compute/virtualMachines', parameters('virtualMachineName'))]"
+            }
+        }
+    ],
+    "outputs": {
+    }
+}
+DEPLOY
+
+  parameters = {
+    "location"             = azurerm_resource_group.main.location
+    "virtualMachineName"   = azurerm_virtual_machine.main.name
+    "autoShutdownStatus"   = var.autoShutdownStatus
+    "autoShutdownTime"     = var.autoShutdownTime
+    "autoShutdownTimeZone" = var.autoShutdownTimeZone
+  }
+
+  deployment_mode = "Incremental"
+
+  depends_on = [
+    azurerm_virtual_machine.main
+  ]
+}
